3e234f9270a48336a1ff466aa21b544535f22996
Dawid <u56128@student.wat.edu.pl>
2025-12-18 02:06:37 +0100

    test: loading progress

--- config/node/frontend/src/App.js
+++ config/node/frontend/src/App.js
@@ -1,11 +1,11 @@
 
 import './styles/style.scss';
-import {Suspense, useState} from "react";
+import {Suspense} from "react";
 import routes from './routes/Router';
 import {RouterProvider} from 'react-router-dom';
-import PageLoader from "/routes/CircularProgress.js";
+import PageLoader from "./routes/CircularProgress.js";
 import './styles/style.scss';
-import React, {useEffect} from 'react';
+
 function App() {
   return (
       <div className="app">
