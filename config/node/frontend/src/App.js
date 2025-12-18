126c5154d273ce1ef2ca61d976146b7822069c5a
Dawid <u56128@student.wat.edu.pl>
2025-12-18 01:31:21 +0100

    test: loading progress

--- config/node/frontend/src/App.js
+++ config/node/frontend/src/App.js
@@ -3,32 +3,14 @@ import './styles/style.scss';
 import {Suspense, useState} from "react";
 import routes from './routes/Router';
 import {RouterProvider} from 'react-router-dom';
-import {CircularProgress} from "@mui/material";
+import {PageLoader} from "/routes/CircularProgress";
 import './styles/style.scss';
 import React, {useEffect} from 'react';
 function App() {
-  const [isLoaded, setIsLoaded] = useState(false);
-  useEffect(() => {
-      if (document.readyState === 'complete') {
-          setIsLoaded(true);
-      } else{
-          const handleLoad = () => setIsLoaded(true);
-          window.addEventListener('load', handleLoad);
-
-          return () => window.removeEventListener('load', handleLoad);
-      }
-  }, []);
-  if (!isLoaded) {
-      return (
-        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: "100vh"}}>
-            <CircularProgress/>
-        </div>
-      );
-  }
   return (
       <div className="app">
           <Suspense
-              fallback={<CircularProgress/>}>
+              fallback={<PageLoader />}>
             <RouterProvider router={routes}/>
         </Suspense>
 
