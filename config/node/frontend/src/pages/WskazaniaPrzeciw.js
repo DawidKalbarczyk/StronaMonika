fbca7698e88be3359cff8a774b8526cb2c18e006
Dawid <u56128@tudent.wat.edu.pl>
2025-12-23 03:52:06 +0100

    fix: fixed JSX syntax

--- config/node/frontend/src/pages/WskazaniaPrzeciw.js
+++ config/node/frontend/src/pages/WskazaniaPrzeciw.js
@@ -4,7 +4,7 @@ import Headbar from "../components/headbar";
 function WskazaniaPrzeciw(props) {
     return (
         <div>
-            <div>{Headbar()}</div>
+            <div><Headbar/></div>
             <div style={{fontSize: 50, color: "white", textAlign: "center"}}>Wskazania i przeciwwskazania</div>
         </div>
     );
