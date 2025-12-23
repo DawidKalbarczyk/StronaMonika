f9c08cb1f432a45b814c51c71ae26558bcb4870b
Dawid <u56128@tudent.wat.edu.pl>
2025-12-23 03:04:26 +0100

    feat: added headbars to other pages, added routing, added all pages

---
+++ config/node/frontend/src/pages/AboutMe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Headbar from "../components/headbar";
+
+function AboutMe(props) {
+    return (
+        <div>
+            <div>{Headbar()}</div>
+            <div style={{fontSize: 50, color: "white", textAlign: "center"}}>O mnie</div>
+        </div>
+    );
+
+}
+
+export default AboutMe;
