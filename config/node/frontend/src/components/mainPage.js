857705e8e1bcd4cd90b512e9d029f4b7855961a4
Dawid <u56128@tudent.wat.edu.pl>
2025-12-25 16:20:55 +0100

    fix: repaired bugs, added main photo to Cennik.js

--- config/node/frontend/src/components/mainPage.js
+++ config/node/frontend/src/components/mainPage.js
@@ -1,37 +1,41 @@
-let lastScrollTop = 0;
+/* let lastScrollTop = 0;
 window.addEventListener('scroll', (event) => {
     let check = window.pageYOffset || document.documentElement.scrollTop;
     if (check > 50) {
         if (check > lastScrollTop) {
-            document.querySelector('.header').classList.add('header-off');
+            document.querySelector('.header')?.classList.add('header-off');
     }
         else if (check < lastScrollTop) {
-            document.querySelector('.header').classList.remove('header-off');
+            document.querySelector('.header')?.classList.remove('header-off');
     }
     }
 
     lastScrollTop = check <= 0 ? 0 : check;
 });
 window.addEventListener("scroll", () => {
-  const scrollY = window.scrollY; // ile px przewinięte od góry
+    const scrollY = window.scrollY; // ile px przewinięte od góry
+    if (window.location.hash !== "/") return;
+    else {
+        if (scrollY >= 300) {
+        document.querySelector(".unique-info-tile-container")?.classList.add("unique-info-tile-container-show");
 
-  if (scrollY >= 300) {
-    document.querySelector(".unique-info-tile-container").classList.add("unique-info-tile-container-show");
-    // tutaj Twoja akcja
+    }
+
   }
 });
 window.addEventListener("scroll", () => {
   const scrollY = window.scrollY; // ile px przewinięte od góry
 
   if (scrollY >= 400) {
-    document.querySelectorAll(".facebook-pic-fixed-container").forEach((element)=> {
+    document.querySelectorAll(".facebook-pic-fixed-container")?.forEach((element)=> {
         element.classList.add("facebook-pic-fixed-container-run");
     });
     // tutaj Twoja akcja
   }
   if (scrollY <= 500) {
-     document.querySelectorAll(".facebook-pic-fixed-container").forEach((element)=> {
+     document.querySelectorAll(".facebook-pic-fixed-container")?.forEach((element)=> {
         element.classList.remove("facebook-pic-fixed-container-run");
     });
   }
 });
+*/
