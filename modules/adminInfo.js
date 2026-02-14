65297340b91896d663791c2a88fe094b010a59b8
Dawid Kalbarczyk <u56128@student.wat.edu.pl>
2026-02-14 19:59:25 +0100

    feat: refactored map.js style to common-styles test: added mapComponent to index.js

--- modules/adminInfo.js
+++ modules/adminInfo.js
@@ -1,5 +1,5 @@
 function AdminInfo() {
-    const html = `
+    /*const html = `
         <footer class="footer bg-colour">
             <div class="footer-content">
                 <span>Administrator strony: <a href="mailto:k_kalbarczyk@o2.pl">k_kalbarczyk@o2.pl</a></span>
@@ -12,6 +12,25 @@ function AdminInfo() {
             </div>
         </footer>
     `
+
+     */
+    const html = `
+        <footer class="admin-info-footer">
+            <div class="admin-info-logo-container">
+                <img src="../photos/logo/logo_obsession_black_transparent.png" alt="admin info logo" class="admin-info-logo-container-logo">
+                <p>- ekspert w kosmetologii. Zadbaj ze mną o swoje piękno świadomie, skutecznie i bezpiecznie!</p>
+            </div>
+            <div class="admin-info-services-container">
+            
+            </div>
+            <div class="admin-info-contact-container">
+            
+            </div>
+            <div class="admin-info-admin-container">
+                
+            </div>
+        </footer>
+    `;
     return (html);
 }
 export default AdminInfo;
