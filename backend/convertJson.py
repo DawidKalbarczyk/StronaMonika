51a90f54ae5c386154b099c75c7426dac5db2854
Dawid <u56128@student.wat.edu.pl>
2026-02-15 19:50:00 +0100

    wip: checking banner update-date

--- backend/convertJson.py
+++ backend/convertJson.py
@@ -1,6 +1,16 @@
 import pandas as pd
 import os
 
+from datetime import datetime
+
+
+now = datetime.now().strftime("%d.%m.%Y, %H:%M:%S")
+
+os.makedirs('data', exist_ok=True)
+
+with open('data/czas.txt', 'w', encoding='utf-8') as f:
+    f.write(now)
+
 script_dir = os.path.dirname(os.path.abspath(__file__))
 path = os.path.join(script_dir, "data", "cennik.xlsx")
 sheet = pd.read_excel(path)
--- modules/adminInfo.js
+++ modules/adminInfo.js
@@ -1,3 +1,5 @@
+import readTime from "./updateTime.js";
+
 function AdminInfo() {
     /*const html = `
         <footer class="footer bg-colour">
@@ -14,6 +16,9 @@ function AdminInfo() {
     `
 
      */
+    const time = readTime();
+
+
     const html = `
         <footer class="admin-info-footer">
             <div class="admin-info-logo-container">
@@ -21,13 +26,24 @@ function AdminInfo() {
                 <p>- ekspert w kosmetologii. Zadbaj ze mną o swoje piękno świadomie, skutecznie i bezpiecznie!</p>
             </div>
             <div class="admin-info-services-container">
-            
+                <p class="admin-info-titles">Moje zabiegi</p>
+                <p class="admin-info-descriptions">
+                
+                </p>
             </div>
             <div class="admin-info-contact-container">
-            
+                <p class="admin-info-titles">Dane kontaktowe</p>
+                <p class="admin-info-descriptions">
+                    Dawid Kalbarczyk<br>
+                    E-mail: k_kalbarczyk@o2.pl<br>
+                    Tel: +48 665 268 517
+                    Ostatnia aktualizacja:<br>
+                    &nbsp;&nbsp;&nbsp;${time}
+                </p>
             </div>
             <div class="admin-info-admin-container">
-                
+                <p class="admin-info-titles">Wykonawca</p>
+                <p class="admin-info-descriptions"></p>
             </div>
         </footer>
     `;
