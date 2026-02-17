aae53d2058b6112417e4b113975d7dbb420adbb0
Dawid <u56128@student.wat.edu.pl>
2026-02-17 01:19:40 +0100

    feat: added form.js to index.js, works as email-sender to owner

--- modules/updateTime.js
+++ modules/updateTime.js
@@ -1,9 +1,9 @@
 async function readTime() {
     const isGitHubPages = window.location.hostname.endsWith("github.io");
     const repoSegment = isGitHubPages ? `/${window.location.pathname.split("/")[1]}` : "";
-    const dataUrl = `${repoSegment}/backend/data/czas.txt`;
+    const dataUrl = `${repoSegment}/backend/data/czas.txt?t=${Date.now()}`;
     
-    const response = await fetch(dataUrl);
+    const response = await fetch(dataUrl, { cache: 'no-store' });
     return await response.text();
 }
 
