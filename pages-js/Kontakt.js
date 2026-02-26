140063ccd375d0a730123c9fdd380ecf2e8c2b49
Dawid Kalbarczyk <u56128@student.wat.edu.pl>
2026-02-26 18:36:54 +0100

    wip: testing scaling with phone

--- pages-js/Kontakt.js
+++ pages-js/Kontakt.js
@@ -118,11 +118,14 @@ function Kontakt() {
 
     `;
     const observer = new IntersectionObserver(entries => {
-        entries.forEach(e => { e.target.classList.toggle('slide-in', e.isIntersecting); });
+        entries.forEach(e => {
+            if (e.isIntersecting) { e.target.classList.add('slide-in'); observer.unobserve(e.target); }
+        });
     }, { threshold: 0.15 });
 
+
     document.querySelectorAll('.social-media-item-container').forEach((el, i) => {
-        el.classList.add('slide-bottom'); el.style.transitionDelay = `${i * 0.1}s`; observer.observe(el);
+        el.classList.add('slide-bottom'); el.style.transitionDelay = `${i * 0.02}s`; observer.observe(el);
     });
     ['.company-details-text-container', '.company-details-photo-container'].forEach((sel, i) => {
         const el = document.querySelector(sel);
